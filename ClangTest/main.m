5ae25830f8f35a2c15328970c6e64213d5a5ddd2
Felix <zyhzhangyunhao@gmail.com>
2019-06-18 08:36:26 +0800

    Initial Commit

---
+++ ClangTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClangTest
+//
+//  Created by Felix on 2019/6/18.
+//  Copyright © 2019 Felix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
